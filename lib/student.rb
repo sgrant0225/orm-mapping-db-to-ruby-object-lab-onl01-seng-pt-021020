cf51dfef7b7735a955bc45baebee112859f74019
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-15 21:30:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,7 +76,8 @@ class Student
    sql = <<-SQL
    SELECT * FROM students WHERE grade > 12  
    SQL
-   DB[:conn].execute(sql)
+   DB[:conn].execute(sql).collect do |i|
+     
    binding.pry
   end
  
