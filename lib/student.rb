cface94e1baf300b7428265367623f321725c296
Sharnell Grant <sharnellgrant@gmail.com>
2020-04-15 21:19:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,8 +73,8 @@ class Student
   end  
   
   def self.students_below_12th_grade 
-   sql =        #<<-SQL 
-   SELECT * FROM students WHERE grade > 12
+   sql = SELECT * FROM students WHERE grade > 12     #<<-SQL 
+   #SELECT * FROM students WHERE grade > 12
    #SQL
    DB[:conn].execute(sql)
    binding.pry
